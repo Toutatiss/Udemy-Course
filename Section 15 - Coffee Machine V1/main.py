bbc8cb76e5f3c8d4778b98abddd6b4823dc0dfdb
Toutatiss <felixkalintsev@gmail.com>
2025-06-04 17:56:41 +1000

    Coffee machine finished, I think!

--- Section 15 - Coffee Machine V1/main.py
+++ Section 15 - Coffee Machine V1/main.py
@@ -28,6 +28,7 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0,
 } 
 
 
@@ -46,49 +47,66 @@ while not validOrder:
     # Prompt user for what coffee they would like
     coffee_input = input("We serve 'espresso', 'latte', and 'cappuccino' coffees. Please enter your order: ")
     ## Check input
-    # Machine status
-    if coffee_input == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-    
-    # Maintenance?
-    if coffee_input == "off": # Switch the machine off, by exiting the ready loop
-
-        print("Machine powering down...")
-        sys.exit()
-    
-    # Valid coffee order?
+     # Valid coffee order?
     for recipe in MENU:
         if coffee_input == recipe:
             coffeeOrder = coffee_input
             validOrder = True
             break
-        
+    # Machine status
+    if coffee_input == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${resources['money']}")
+    elif coffee_input == "off": # Switch the machine off, by exiting the ready loop
+        print("Machine powering down...")
+        sys.exit() 
+         
     if not validOrder and coffee_input != "report":
-        print("That's not a valid coffee order, please try again.")
-        
+        print("That's not a valid coffee order, please try again.")   
     # Check for sufficient resources
-    for ingredient, quantityRequired in MENU[coffeeOrder]["ingredients"].items():
-        quantityAvailable = resources.get(ingredient, 0)
-        if quantityRequired > quantityAvailable:
-            print(f"Sorry, there is not enough {ingredient}.")
+    elif validOrder and coffee_input != "report":
+        for ingredient, quantityRequired in MENU[coffeeOrder]["ingredients"].items():
+            quantityAvailable = resources.get(ingredient, 0)
+            if quantityRequired > quantityAvailable:
+                print(f"Sorry, there is not enough {ingredient}.")
+                validOrder = False
+                # print(f"{ingredient}: required = {quantityRequired}, available = {quantityAvailable}")
+    
+    if validOrder:            
+        # Request Coins
+        print("Please insert your coins.")
+        quarter_input = int(input("How many quarters?: "))
+        dimes_input = int(input("How many dimes?: "))
+        nickles_input = int(input("How many nickles?: "))
+        pennies_input = int(input("How many pennies?: "))
+        
+        available_funds = quarter_input*0.25 + dimes_input*0.1 + nickles_input*0.05 + pennies_input*0.01
+        
+        order_price = MENU[coffeeOrder]["cost"]
+        
+        if available_funds < order_price:
+            print("Insufficient funds! Money Refunded.")
+            available_funds = 0
             validOrder = False
-            # print(f"{ingredient}: required = {quantityRequired}, available = {quantityAvailable}")
-            
-    # Request Coins
-    print("Please insert your coins.")
-    quarter_input = int(input("How many quarters?: "))
-    dimes_input = int(input("How many dimes?: "))
-    nickles_input = int(input("How many nickles?: "))
-    pennies_input = int(input("How many pennies?: "))
-    
-    available_funds = quarter_input*0.25 + dimes_input*0.1 + nickles_input*0.05 + pennies_input*0.01
-    
-    if available_funds < MENU[coffeeOrder]["cost"]:
-        print("Insufficient funds!")
-    else:
-        print("Sufficent funds!")
+        else:
+            # Process funds
+            resources["money"] += order_price
+            available_funds = available_funds - order_price
+            if available_funds > 0:
+                print(f"Here is your change: ${available_funds:.2f}")
+                available_funds = 0
+            # Make coffee
+            for ingredient, quantityRequired in MENU[coffeeOrder]["ingredients"].items():
+                resources[ingredient] -= quantityRequired
+            print("Here is your order. Thank you for purchase!")
+            validOrder = False
+
+        
+        
+        
+        
     
 
 
