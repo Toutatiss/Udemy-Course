7a61e4cb0cb8dd65a737dd4e7ffeffdfc6dceb12
Toutatiss <felixkalintsev@gmail.com>
2025-06-10 14:30:41 +1000

    wall and paddle collisions fixed - no double bounce, no set rectangular path

--- Section 22 - Pong/ball.py
+++ Section 22 - Pong/ball.py
@@ -1,14 +1,26 @@
 from turtle import Turtle
+import time
 
 MOVE_DISTANCE = 10
 
-class Ball():
+class Ball(Turtle):
     def __init__(self):
-        self.ball = Turtle()
-        self.ball.penup()
-        self.ball.shape("circle")
-        self.ball.color("blue")
-        self.ball.setheading(30)
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("blue")
+        self.setheading(30)
+        self.last_bounced_time = 0
         
     def move(self):
-        self.ball.forward(MOVE_DISTANCE)
+        self.forward(MOVE_DISTANCE)
+        
+    def bounce(self, obstacle_orientation):
+        if obstacle_orientation == "vertical":
+            new_heading = (180 - self.heading()) % 360
+        elif obstacle_orientation == "horizontal":
+            new_heading = (-self.heading()) % 360
+            
+        self.setheading(new_heading)
+        self.last_bounced_time = time.time()
+        print("boing")
--- Section 22 - Pong/main.py
+++ Section 22 - Pong/main.py
@@ -8,11 +8,16 @@ from ball import Ball
 import time
 
 # Constants
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-RIGHT_PADDLE_STARTING_COORDS = [(280,30), (280, 10), (280, -10)]
-LEFT_PADDLE_STARTING_COORDS = [(-290,30), (-290, 10), (-290, -10)]
+VERTICAL_BOUNDARY = SCREEN_HEIGHT/2 - 25
+HORIZONTAL_BOUNDARY = SCREEN_WIDTH / 2
+RIGHT_X = HORIZONTAL_BOUNDARY - 35
+LEFT_X = -HORIZONTAL_BOUNDARY + 25
+
+RIGHT_PADDLE_STARTING_COORDS = [(RIGHT_X,30), (RIGHT_X, 10), (RIGHT_X, -10)]
+LEFT_PADDLE_STARTING_COORDS = [(LEFT_X,30), (LEFT_X, 10), (LEFT_X, -10)]
 
 # Create the game scene
 screen = Screen()
@@ -36,14 +41,39 @@ screen.onkeypress(right_paddle.down, "Down")
 screen.onkeypress(left_paddle.up, "w")
 screen.onkeypress(left_paddle.down, "s")
 
+def detect_paddle_collision(paddle):
+    for segment in paddle.segments:
+        time_between_bounces = current_time - ball.last_bounced_time
+        if segment.distance(ball) < 25 and time_between_bounces > 0.2:
+            ball.bounce("vertical")
+            
+def detect_wall_collision():
+    time_between_bounces = current_time - ball.last_bounced_time
+    if (ball.ycor() > VERTICAL_BOUNDARY or ball.ycor() < -VERTICAL_BOUNDARY) and time_between_bounces > 0.2:
+        ball.bounce("horizontal")
+
 game_running = True
 
 # Wait a second before starting the ball moving
 time.sleep(1)
+start_time = time.time()
 
 while game_running:
     screen.update()
     ball.move()
     time.sleep(0.05)
+    
+    current_time = time.time()
+    
+    detect_paddle_collision(right_paddle)
+    detect_paddle_collision(left_paddle)
+
+    # Wall collision
+    detect_wall_collision()
+    
+    # Out of bounds
+    if ball.xcor() > HORIZONTAL_BOUNDARY or ball.xcor() < -HORIZONTAL_BOUNDARY:
+        pass
+
 
 screen.exitonclick()
